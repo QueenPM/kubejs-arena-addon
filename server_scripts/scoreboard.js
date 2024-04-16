e3e6a4a2488a967f41fae586f3ed3d4566825ced
QueenPM <danimark644@gmail.com>
2024-04-16 21:11:06 +0100

    refactor: scoreboard.js commented out because classes are not supported in kubejs

--- server_scripts/scoreboard.js
+++ server_scripts/scoreboard.js
@@ -1,36 +1,36 @@
-/**
- * Class to help with Custom Scoreboards
- * https://www.digminecraft.com/game_commands/scoreboard_command.php
- */
-class CustomScoreboard{
-    /**
-     * @param {string} objective Name of the scoreboard. No Spaces. This is used as an identifier in Minecraft
-     * @param {string} displayName Display Name of the scoreboard.
-     * @param {string[]} lines Array of lines to display on the scoreboard.
-     */
-    constructor(objective, displayName,lines = []){
-        this.objective = objective;
-        this.lines = lines;
-        this.displayName = displayName
-    }
+// /**
+//  * Class to help with Custom Scoreboards
+//  * https://www.digminecraft.com/game_commands/scoreboard_command.php
+//  */
+// class CustomScoreboard{
+//     /**
+//      * @param {string} objective Name of the scoreboard. No Spaces. This is used as an identifier in Minecraft
+//      * @param {string} displayName Display Name of the scoreboard.
+//      * @param {string[]} lines Array of lines to display on the scoreboard.
+//      */
+//     constructor(objective, displayName,lines = []){
+//         this.objective = objective;
+//         this.lines = lines;
+//         this.displayName = displayName
+//     }
 
-    /**
-     * Create the scoreboard on the Minecraft Server
-     */
-    createScoreboard(){
-        let server = Utils.getServer();
-        if(!server) return;
-        server.runCommandSilent(`scoreboard objectives add ${this.objective} dummy ${this.displayName}`)
-    }
+//     /**
+//      * Create the scoreboard on the Minecraft Server
+//      */
+//     createScoreboard(){
+//         let server = Utils.getServer();
+//         if(!server) return;
+//         server.runCommandSilent(`scoreboard objectives add ${this.objective} dummy ${this.displayName}`)
+//     }
 
-    /**
-     * 
-     * @param {"belowName"|"list"|"sidebar"} display - Minecraft setdisplay
-     * @returns 
-     */
-    setDisplay(display){
-        let server = Utils.getServer();
-        if(!server) return;
-        server.runCommandSilent(`scoreboard objectives setdisplay ${display} ${this.objective}`)
-    }
-}
+//     /**
+//      * 
+//      * @param {"belowName"|"list"|"sidebar"} display - Minecraft setdisplay
+//      * @returns 
+//      */
+//     setDisplay(display){
+//         let server = Utils.getServer();
+//         if(!server) return;
+//         server.runCommandSilent(`scoreboard objectives setdisplay ${display} ${this.objective}`)
+//     }
+// }
