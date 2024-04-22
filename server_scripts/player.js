5828dd76b18c2d61e46031af36da074830957c03
QueenPM <danimark644@gmail.com>
2024-04-22 19:26:38 +0100

    fix: give knockback resistance on respawn

--- server_scripts/player.js
+++ server_scripts/player.js
@@ -30,5 +30,9 @@ PlayerEvents.loggedIn((e)=>{
 
     leaveTeam(e.player);
     e.server.runCommandSilent(`attribute ${e.player.username} minecraft:generic.knockback_resistance base set 10`)
-    e.server.runCommandSilent(`attribute ${e.player.username} minecraft:generic.armor base set 15`)
+})
+
+
+PlayerEvents.respawned((e)=>{
+    e.server.runCommandSilent(`attribute ${e.player.username} minecraft:generic.knockback_resistance base set 10`)
 })
